29c4d924731314b427940fbf9cbca5ac4916df40
Kanisorn S <firstziiz.k@gmail.com>
2018-01-03 02:11:22 +0700

    reset complete

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,27 @@
 const machineEl = document.createElement('machine')
 const wheelCount = 1;
 const fib = [1,1,1,1,1,1,1,1,1,1]
-const slideCount = fib.reduce((a,b)=> a + b, 0);
-const slideAngle = 360 / slideCount;
 const wheels = Array(wheelCount)
 
-createSlots(["A", "B", "C", "D", "E", "F", "G", "H", "i", "J"])
+let Item = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
 
-function createSlots(emojis){
+createSlots(Item)
+
+function reset(item) {
+  if (Item.indexOf(item) !== -1 ) {
+    Item.splice(Item.indexOf(item), 1 );
+    fib.pop()
+    console.log(fib.length, Item.length)
+    $('machine').html('')
+    createSlots(Item)
+  }
+}
+
+function createSlots(itemList){
+  const slideCount = fib.reduce((a,b)=> a + b, 0);
+  const slideAngle = 360 / slideCount;
+  const defAngle = fib.length % 2 === 0 ? 180 : 360
+  
   const play = function(){
     let wheelsEls = $('wheel')
     wheelsEls.addClass('active')
@@ -21,9 +35,10 @@ function createSlots(emojis){
     });
 
     for (let i = 0; i < wheels.length; i++){
-      let degreesToMove = Math.floor( (Math.random() + 3) * slideCount + slideCount) * slideAngle; 
+      let degreesToMove = Math.floor( 2 * slideCount + slideCount) * slideAngle; 
       wheelsEls[i].rotation -= degreesToMove;
       wheelsEls[i].setAttribute('style', 'transform: translateZ(-82vmax) rotateY('+wheelsEls[i].rotation+'deg)  ');
+      checkChoosen(wheelsEls[i].rotation)
     }
   };
 
@@ -34,21 +49,35 @@ function createSlots(emojis){
     wheelEl.rotation = 0;
     wheel = new Array(slideCount)
     machineEl.appendChild(wheelEl)
-    let emojiIndex = 0;
+    let enjiIndex = 0;
     let slideIndex = 0
-    for (let emoji of emojis){
-      let emojiCount = fib[emojiIndex]
-      emojiIndex++
-      while (emojiCount > 0){
+    for (let enji of itemList){
+      let enjiCount = fib[enjiIndex]
+      enjiIndex++
+      while (enjiCount > 0){
         let slideEl = document.createElement('slide')
-        slideEl.classList.add('emoji')
-        slideEl.setAttribute("style", "transform: rotateY(" + (slideIndex * slideAngle - 180) + "deg) translateZ(80vmax) ;")
-        slideEl.textContent = emoji;
+        slideEl.classList.add('enji')
+        slideEl.setAttribute("style", "transform: rotateY(" + (slideIndex * slideAngle - defAngle) + "deg) translateZ(80vmax) ;")
+        slideEl.setAttribute("data-deg", slideIndex * slideAngle - defAngle)
+        slideEl.setAttribute("data-team", enji)
+        slideEl.textContent = enji;
         wheelEl.appendChild(slideEl);
-        emojiCount--
+        enjiCount--
         slideIndex++
       }
     }
+  }
+
+  const checkChoosen = function(deg) {
+    const data = $('slide').map((index,item) => {
+      const raw = $(item).data('deg')
+      if ( ((deg % -360 ) + 180 + 36) % 180 === +raw) {
+        console.log($(item).data('team'))
+        return $(item).data('team')
+      } else {
+        console.log('none')
+      }
+    })
   }
   
   $('body').append(machineEl)
